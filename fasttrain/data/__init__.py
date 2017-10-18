d8395e8915421f82446e9b8d55fcc6913acbe706
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-18 18:47:00 -0400

    minor

--- fasttrain/data/__init__.py
+++ fasttrain/data/__init__.py
@@ -1,9 +1,8 @@
 from torchvision.datasets import CIFAR10
 
+
 DATA_PATH = './data'
 
-cifar_train = CIFAR10(DATA_PATH, train=True, download=True)
-cifar_test = CIFAR10(DATA_PATH, train=False, download=True)
 
-print(len(cifar_test))
-print(len(cifar_train))
+def load_cifar10(train=True):
+    return CIFAR10(DATA_PATH, train=train, download=True)
