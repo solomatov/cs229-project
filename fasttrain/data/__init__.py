1d8effa7f9cabd71e01b8ce91d1f903bf57ad03a
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-19 09:33:20 -0400

    data augmentation in the pipeline

--- fasttrain/data/__init__.py
+++ fasttrain/data/__init__.py
@@ -1,8 +1,13 @@
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
 
 DATA_PATH = './data'
 
 def load_cifar10(train=True):
-    return CIFAR10(DATA_PATH, train=train, download=True, transform=ToTensor())
+    return CIFAR10(DATA_PATH, train=train, download=True, transform=transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(size=(32, 32), padding=4),
+            transforms.ToTensor()
+        ]))
 
