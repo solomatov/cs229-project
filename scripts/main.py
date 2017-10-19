7352451fbab0c3ea8c9c9665ece14e648fc94f47
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-19 01:46:31 -0400

    change epochs to 10

--- scripts/main.py
+++ scripts/main.py
@@ -7,7 +7,7 @@ train = load_cifar10(train=True)
 test = load_cifar10(train=False)
 
 runner = Runner(net, train, batch_size=128)
-runner.run(epochs=50)
+runner.run(epochs=10)
 
 print('Test accuracy: {}'.format(runner.evaluate(test)))
 print('Train accuracy: {}'.format(runner.evaluate(train)))
