51707a7f6c348f021bef5be4b3f6d6e57087f6f0
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-19 00:52:10 -0400

    tqdm

--- fasttrain/main.py
+++ fasttrain/main.py
@@ -3,35 +3,39 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from fasttrain.data import load_cifar10
 from fasttrain.model import NaiveCNN
 
 
+
 class Runner:
-    def __init__(self, net, loader, batch_size=128, use_cuda=torch.cuda.is_available(), loss_fun=nn.CrossEntropyLoss()):
+    def __init__(self, net, dataset, batch_size=128, use_cuda=torch.cuda.is_available(), loss_fun=nn.CrossEntropyLoss()):
         self.__net = net
-        self.__loader = loader
+        self.__dataset = dataset
         self.__batch_size = batch_size
         self.__use_cuda = use_cuda
         self.__loss_fun = loss_fun
 
-    def run(self, epochs=1):
-        net = self.__net
         if self.__use_cuda:
-            net = net.cuda()
+            self.__net = self.__net.cuda()
 
-        optimizer = optim.Adam(net.parameters(), lr=1e-4)
-        loss_fun = nn.CrossEntropyLoss()
+    def run(self, epochs=1):
+        optimizer = optim.Adam(self.__net.parameters(), lr=1e-4)
+        loader = DataLoader(self.__dataset, batch_size=self.__batch_size, num_workers=2)
 
         for e in range(epochs):
             print('Epoch = {}'.format(e))
-            for i, data in enumerate(self.__loader, 0):
+
+            t = tqdm(total=len(loader))
+
+            for i, data in enumerate(loader, 0):
                 X_batch, y_batch = self.__opt(data[0]), self.__opt(data[1])
                 X_var, y_var = Variable(X_batch), Variable(y_batch)
 
                 optimizer.zero_grad()
-                y_ = net(X_var)
+                y_ = self.__net(X_var)
 
                 loss = self.__loss_fun(y_, y_var)
                 loss.backward()
@@ -39,9 +43,26 @@ class Runner:
 
                 _, y_ = torch.max(y_, dim=1)
 
-                print(self.__accuracy(y_var, y_))
+                t.update(1)
 
-                print('Loss = {}'.format(loss.data[0]))
+                t.set_postfix_str('{:.1f}% loss={:.2f}'.format(100.0 * self.__accuracy(y_var, y_), loss.data[0]))
+            t.close()
+
+    def evaluate(self, data):
+        loader = DataLoader(data, batch_size=128, num_workers=2)
+
+        samples = 0
+        total_correct = 0
+
+        for i, data in enumerate(loader, 0):
+            X_batch, y_batch = self.__opt(data[0]), self.__opt(data[1])
+            X_var, y_var = Variable(X_batch), Variable(y_batch)
+            y_ = torch.max(self.__net(X_var), dim=1)[1]
+
+            samples += y_batch.size()[0]
+            total_correct += torch.sum(torch.eq(y_, y_var)).data[0]
+
+        return total_correct / samples
 
     def __opt(self, t):
         if self.__use_cuda:
@@ -52,19 +73,12 @@ class Runner:
         return torch.mean(torch.eq(y_, y).type(torch.FloatTensor)).data[0]
 
 
-has_cuda = torch.cuda.is_available()
-
 net = NaiveCNN()
-
-if has_cuda:
-    net = net.cuda()
-
 train = load_cifar10(train=True)
 test = load_cifar10(train=False)
 
-train_loader = DataLoader(train, batch_size=128, num_workers=2)
-optimizer = optim.Adam(net.parameters(), lr=0.001)
-lossfun = nn.CrossEntropyLoss()
-
-runner = Runner(net, train_loader, batch_size=128)
+runner = Runner(net, train, batch_size=128)
 runner.run()
+
+print('Test accuracy: {}'.format(runner.evaluate(test)))
+print('Train accuracy: {}'.format(runner.evaluate(train)))
