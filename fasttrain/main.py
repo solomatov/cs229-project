b7b318a372d524aa3a7751b8d1d8d10a1768dc52
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-19 00:12:36 -0400

    extract runner

--- fasttrain/main.py
+++ fasttrain/main.py
@@ -10,6 +10,44 @@ import numpy as np
 from fasttrain.model import NaiveCNN
 from fasttrain.data import load_cifar10
 
+
+class Runner:
+    def __init__(self, net, loader, batch_size=128, use_cuda=torch.cuda.is_available(), loss_fun=nn.CrossEntropyLoss()):
+        self.__net = net
+        self.__loader = loader
+        self.__batch_size = batch_size
+        self.__use_cuda = use_cuda
+        self.__loss_fun = loss_fun
+
+    def run(self, epochs=1):
+        net = self.__net
+        if self.__use_cuda:
+            net = net.cuda()
+
+        optimizer = optim.Adam(net.parameters(), lr=1e-4)
+        loss_fun = nn.CrossEntropyLoss()
+
+        for e in range(epochs):
+            print('Epoch = {}'.format(e))
+            for i, data in enumerate(self.__loader, 0):
+                X_batch, y_batch = self.optimize(data[0]), self.optimize(data[1])
+                X_var, y_var = Variable(X_batch), Variable(y_batch)
+
+                optimizer.zero_grad()
+                out_ = net(X_var)
+
+                loss = self.__loss_fun(out_, y_var)
+                loss.backward()
+                optimizer.step()
+
+                print('Loss = {}'.format(loss.data[0]))
+
+    def optimize(self, t):
+        if self.__use_cuda:
+            return t.cuda()
+        return t
+
+
 has_cuda = torch.cuda.is_available()
 
 net = NaiveCNN()
@@ -24,23 +62,5 @@ train_loader = DataLoader(train, batch_size=128, num_workers=2)
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 lossfun = nn.CrossEntropyLoss()
 
-for e in range(10):
-    print('Epoch={0}'.format(e))
-
-    for i, data in enumerate(train_loader, 0):
-        X, y = data
-
-        if has_cuda:
-            X, y = X.cuda(), y.cuda()
-
-        inp, out = Variable(X), Variable(y)
-
-        optimizer.zero_grad()
-        out_ = net(inp)
-        loss = lossfun(out_, out)
-        loss.backward()
-        optimizer.step()
-        print('Loss = {}'.format(loss.data[0]))
-
-        print(np.mean(out_.data.cpu().numpy().argmax(axis=1) == out.data.cpu().numpy()))
-
+runner = Runner(net, train_loader, batch_size=128)
+runner.run()
