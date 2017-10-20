567b18622afa86b627bd97868391399d02466d89
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-19 21:48:21 -0400

    no crop in test eval

--- fasttrain/data/__init__.py
+++ fasttrain/data/__init__.py
@@ -3,11 +3,12 @@ import torchvision.transforms as transforms
 
 DATA_PATH = './data'
 
+
 def load_cifar10(train=True):
-    return CIFAR10(DATA_PATH, train=train, download=True, transform=transforms.Compose(
-        [
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(size=(32, 32), padding=4),
-            transforms.ToTensor()
-        ]))
+    if train:
+        transformers = [transforms.RandomHorizontalFlip(), transforms.RandomCrop(size=(32, 32), padding=8)]
+    else:
+        transformers = []
+    transformers.append(transforms.ToTensor())
+    return CIFAR10(DATA_PATH, train=train, download=True, transform=transforms.Compose(transformers))
 
