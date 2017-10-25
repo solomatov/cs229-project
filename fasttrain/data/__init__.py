d447d1a9834436a2f79caf22308f133f7a8d1f7f
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-25 16:16:43 -0400

    padding = 4

--- fasttrain/data/__init__.py
+++ fasttrain/data/__init__.py
@@ -20,7 +20,7 @@ class SublistDataset(Dataset):
 def load_cifar10(train=True, data_path='./data'):
     transformers = []
     if train:
-        transformers.extend([transforms.RandomHorizontalFlip(), transforms.RandomCrop(size=(32, 32), padding=8)])
+        transformers.extend([transforms.RandomHorizontalFlip(), transforms.RandomCrop(size=(32, 32), padding=4)])
     transformers.append(transforms.ToTensor())
     transformers.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
     return CIFAR10(data_path, train=train, download=True, transform=transforms.Compose(transformers))
