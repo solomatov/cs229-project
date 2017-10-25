4a1e3c1ecc1ab90e868e1b6aa6c433ec0ef7f664
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-25 13:15:14 -0400

    resnet_20_lr for parameter tuning

--- scripts/resnet_20_lr.py
+++ scripts/resnet_20_lr.py
@@ -4,6 +4,6 @@ import torch.optim as optim
 from fasttrain.model import ResNetCIFAR
 from fasttrain import train_on_cifar
 
-for lr in np.logspace(-10, -4, 7):
+for lr in np.logspace(-8, -4, 5):
     print('lr = {}'.format(lr))
     train_on_cifar(ResNetCIFAR(20), batch_size=10, epochs=20, use_all_gpus=False, opt_factory=lambda p: optim.Adam(p, lr=lr))
