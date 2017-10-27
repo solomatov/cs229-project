0b76d081ac0f0918e347bacd4ea233f3971749ac
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-10-26 21:51:06 -0400

    remove unnecessary scripts

--- scripts/resnet_20_lr.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import torch.optim as optim
-
-from fasttrain.model import ResNetCIFAR
-from fasttrain import train_on_cifar
-
-for lr in np.logspace(-8, -4, 5):
-    print('lr = {}'.format(lr))
-    train_on_cifar(ResNetCIFAR(20), batch_size=128, epochs=10, opt_factory=lambda p: optim.Adam(p, lr=lr))
