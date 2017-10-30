1b5cf08a7f85ccba0959c8a27e7076acd084e5b1
Denis Stepanov <denis_v_stepanov@hotmail.com>
2017-10-30 15:30:25 +0300

    Preactivated version of ResNet (https://arxiv.org/pdf/1603.05027.pdf) added: simple and down block classes refactored, ResNetCIFAR initialized with original simple and down blocks by default.

--- fasttrain/model/resnet.py
+++ fasttrain/model/resnet.py
@@ -9,11 +9,15 @@ def make_conv(channels):
 class SimpleBlock(nn.Module):
     def __init__(self, channels):
         super(SimpleBlock, self).__init__()
-
         self.conv1 = make_conv(channels)
         self.bn1 = nn.BatchNorm2d(channels)
         self.conv2 = make_conv(channels)
         self.bn2 = nn.BatchNorm2d(channels)
+
+
+class OriginalSimpleBlock(SimpleBlock):
+    def __init__(self, channels):
+        super(OriginalSimpleBlock, self).__init__(channels)
 
     def forward(self, x):
         c1 = F.relu(self.bn1(self.conv1(x)))
@@ -21,20 +25,31 @@ class SimpleBlock(nn.Module):
         return F.relu(x + c2)
 
 
+class PreactivatedSimpleBlock(SimpleBlock):
+    def __init__(self, channels):
+        super(PreactivatedSimpleBlock, self).__init__(channels)
+
+    def forward(self, x):
+        c1 = self.conv1(F.relu(self.bn1(x)))
+        c2 = self.conv2(F.relu(self.bn2(c1)))
+        return x + c2
+
+
 class DownBlock(nn.Module):
     def __init__(self, in_chan):
         super(DownBlock, self).__init__()
+        self.out_chan = in_chan * 2
+        self.conv1 = nn.Conv2d(in_chan, self.out_chan, kernel_size=(3, 3), stride=2, padding=1)
+        self.conv2 = make_conv(self.out_chan)
+        self.bn2 = nn.BatchNorm2d(self.out_chan)
+        self.conv_down = nn.Conv2d(in_chan, self.out_chan, kernel_size=(1, 1), stride=2, bias=False)
+        self.bn_down = nn.BatchNorm2d(self.out_chan)
 
-        out_chan = in_chan * 2
-        self.conv1 = nn.Conv2d(in_chan, out_chan, kernel_size=(3, 3), stride=2, padding=1)
-        self.bn1 = nn.BatchNorm2d(out_chan)
-        self.conv2 = make_conv(out_chan)
-        self.bn2 = nn.BatchNorm2d(out_chan)
-        self.conv_down = nn.Conv2d(in_chan, out_chan, kernel_size=(1, 1), stride=2, bias=False)
-        self.bn_down = nn.BatchNorm2d(out_chan)
 
-        self.bn_down.weight.data.fill_(1)
-        self.bn_down.bias.data.fill_(0)
+class OriginalDownBlock(DownBlock):
+    def __init__(self, in_chan):
+        super(OriginalDownBlock, self).__init__(in_chan)
+        self.bn1 = nn.BatchNorm2d(self.out_chan)
 
     def forward(self, x):
         c1 = F.relu(self.bn1(self.conv1(x)))
@@ -43,29 +58,43 @@ class DownBlock(nn.Module):
         return F.relu(down + c2)
 
 
+class PreactivatedDownBlock(DownBlock):
+    def __init__(self, in_chan):
+        super(PreactivatedDownBlock, self).__init__(in_chan)
+        self.bn1 = nn.BatchNorm2d(in_chan)
+
+    def forward(self, x):
+        c1 = self.conv1(F.relu(self.bn1(x)))
+        c2 = self.conv2(F.relu(self.bn2(c1)))
+        down = self.bn_down(self.conv_down(x))
+        return down + c2
+
+
 class ResNetCIFAR(nn.Module):
-    def __init__(self, n):
+    def __init__(self, n, simple_block=OriginalSimpleBlock, down_block=OriginalDownBlock):
         super(ResNetCIFAR, self).__init__()
+        self.SimpleBlock = simple_block
+        self.DownBlock = down_block
         self.conv1 = nn.Conv2d(3, 16, (3, 3), stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
 
         self.seq32_32 = nn.Sequential()
         for i in range(n):
-            self.seq32_32.add_module(str(i), SimpleBlock(16))
+            self.seq32_32.add_module(str(i), self.SimpleBlock(16))
 
         self.seq16_16 = nn.Sequential()
         for i in range(n):
             if i == 0:
-                self.seq16_16.add_module(str(i), DownBlock(16))
+                self.seq16_16.add_module(str(i), self.DownBlock(16))
             else:
-                self.seq16_16.add_module(str(i), SimpleBlock(32))
+                self.seq16_16.add_module(str(i), self.SimpleBlock(32))
 
         self.seq8_8 = nn.Sequential()
         for i in range(n):
             if i == 0:
-                self.seq8_8.add_module(str(i), DownBlock(32))
+                self.seq8_8.add_module(str(i), self.DownBlock(32))
             else:
-                self.seq8_8.add_module(str(i), SimpleBlock(64))
+                self.seq8_8.add_module(str(i), self.SimpleBlock(64))
 
         self.fc = nn.Linear(64, 10)
 
@@ -80,6 +109,3 @@ class ResNetCIFAR(nn.Module):
         flat = features.view(features.size()[0], -1)
 
         return self.fc(flat)
-
-
-
