63f4c89b2bbebb96c40b8d83e20a924c0cf15704
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-11-11 19:39:03 -0500

    work on stochastic depth

--- fasttrain/model/resnet.py
+++ fasttrain/model/resnet.py
@@ -1,3 +1,4 @@
+import random
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -7,27 +8,31 @@ def make_conv(channels):
 
 
 class SimpleBlock(nn.Module):
-    def __init__(self, channels, pre_activated=False):
+    def __init__(self, channels, pre_activated=False, prob=1.0):
         super(SimpleBlock, self).__init__()
         self.conv1 = make_conv(channels)
         self.bn1 = nn.BatchNorm2d(channels)
         self.conv2 = make_conv(channels)
         self.bn2 = nn.BatchNorm2d(channels)
         self.pre_activated = pre_activated
+        self.prob = prob
 
     def forward(self, x):
+        if self.training and random.random() > self.prob:
+            return x
+
         if self.pre_activated:
             c1 = self.conv1(F.relu(self.bn1(x)))
             c2 = self.conv2(F.relu(self.bn2(c1)))
-            return x + c2
+            return x + self.prob * c2
         else:
             c1 = F.relu(self.bn1(self.conv1(x)))
             c2 = self.bn2(self.conv2(c1))
-            return F.relu(x + c2)
+            return F.relu(x + self.prob * c2)
 
 
 class DownBlock(nn.Module):
-    def __init__(self, in_chan, pre_activated=False):
+    def __init__(self, in_chan, pre_activated=False, prob=1.0):
         super(DownBlock, self).__init__()
         self.out_chan = in_chan * 2
         self.conv1 = nn.Conv2d(in_chan, self.out_chan, kernel_size=(3, 3), stride=2, padding=1)
@@ -39,6 +44,7 @@ class DownBlock(nn.Module):
         self.bn_down.weight.data.fill_(1)
         self.bn_down.bias.data.fill_(0)
 
+        self.prob = prob
         self.pre_activated = pre_activated
 
         if self.pre_activated:
@@ -48,19 +54,25 @@ class DownBlock(nn.Module):
 
     def forward(self, x):
         if self.pre_activated:
+            if self.training and random.random() > self.prob:
+                return self.bn_down(self.conv_down(x))
+
             c1 = self.conv1(F.relu(self.bn1(x)))
             c2 = self.conv2(F.relu(self.bn2(c1)))
             down = self.bn_down(self.conv_down(x))
-            return down + c2
+            return down + self.prob * c2
         else:
+            if self.training and random.random() > self.prob:
+                return F.relu(self.bn_down(self.conv_down(x)))
+
             c1 = F.relu(self.bn1(self.conv1(x)))
             c2 = self.bn2(self.conv2(c1))
             down = self.bn_down(self.conv_down(x))
-            return F.relu(down + c2)
+            return F.relu(down + self.prob * c2)
 
 
 class ResNetCIFAR(nn.Module):
-    def __init__(self, n, pre_activated=False):
+    def __init__(self, n, pre_activated=False, stochastic_depth=False):
         super(ResNetCIFAR, self).__init__()
 
         self.pre_activated = pre_activated
@@ -68,23 +80,28 @@ class ResNetCIFAR(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, (3, 3), stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
 
+        if stochastic_depth:
+            prob = 0.5
+        else:
+            prob = 1.0
+
         self.seq32_32 = nn.Sequential()
         for i in range(n):
-            self.seq32_32.add_module(str(i), SimpleBlock(16))
+            self.seq32_32.add_module(str(i), SimpleBlock(16, prob=prob))
 
         self.seq16_16 = nn.Sequential()
         for i in range(n):
             if i == 0:
-                self.seq16_16.add_module(str(i), DownBlock(16))
+                self.seq16_16.add_module(str(i), DownBlock(16, prob=prob))
             else:
-                self.seq16_16.add_module(str(i), SimpleBlock(32))
+                self.seq16_16.add_module(str(i), SimpleBlock(32, prob=prob))
 
         self.seq8_8 = nn.Sequential()
         for i in range(n):
             if i == 0:
-                self.seq8_8.add_module(str(i), DownBlock(32))
+                self.seq8_8.add_module(str(i), DownBlock(32, prob=prob))
             else:
-                self.seq8_8.add_module(str(i), SimpleBlock(64))
+                self.seq8_8.add_module(str(i), SimpleBlock(64, prob=prob))
 
         self.fc = nn.Linear(64, 10)
 
