56e75feb20e366f70efa662ae6124a6bf31e05bd
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-11-17 15:32:04 -0500

    pre activation option

--- scripts/resnet_stacked.py
+++ scripts/resnet_stacked.py
@@ -8,6 +8,7 @@ parser.add_argument('-b', '--batch-size', type=int, default=128)
 parser.add_argument('-lr', '--learn_rate', type=float, default=0.1)
 parser.add_argument('-sd', '--stochastic-depth', type=str, default=None)
 parser.add_argument('-st', '--show-test', type=bool, default=False)
+parser.add_argument('-pa', '--pre-activated', type=bool, default=False)
 
 args = parser.parse_args()
 
@@ -27,4 +28,5 @@ train_stacked(args.number,
               batch_size=args.batch_size,
               stochastic_depth=stochastic_depth,
               show_test=args.show_test,
+              pre_activated=args.pre_activated,
               base_lr=args.learn_rate)
