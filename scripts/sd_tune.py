3212e05a8458cbb66a660313fc63aeeb23e1936a
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-11-19 18:22:47 -0500

    minor

--- scripts/sd_tune.py
+++ scripts/sd_tune.py
@@ -8,6 +8,7 @@ from fasttrain.schedules import resnet_paper_schedule
 batch_size = 3072
 schedule = resnet_paper_schedule(batch_size=batch_size)
 
-for prob in np.linspace(0.1, 1.0, 10):
+for prob in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+    print(prob)
     model = ResNetCIFAR(n=20, stochastic_depth={'from': prob, 'to': prob})
     train_on_cifar(model, schedule, name=f'ResNet(sd-p={prob})', batch_size=batch_size)
