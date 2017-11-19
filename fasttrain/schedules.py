fe532bc578ccdd206420be6d3fb2e570c07337e3
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-11-19 13:48:45 -0500

    include warmup into the schedule

--- fasttrain/schedules.py
+++ fasttrain/schedules.py
@@ -3,13 +3,26 @@ from fasttrain.framework import TrainSchedule
 import torch.optim as optim
 
 
-def resnet_paper_schedule():
+def resnet_paper_schedule(batch_size=128):
     schedule = TrainSchedule()
+
     wd = 0.0001
     momentum = 0.9
 
-    schedule.add_step(factory=lambda p: optim.SGD(p, lr=0.01, weight_decay=wd, momentum=momentum), name='P1', duration=80)
-    schedule.add_step(factory=lambda p: optim.SGD(p, lr=0.001, weight_decay=wd, momentum=momentum), name='P2', duration=80)
-    schedule.add_step(factory=lambda p: optim.SGD(p, lr=0.0001, weight_decay=wd, momentum=momentum), name='P3', duration=40)
+    def new_optim(p, lr):
+        return optim.SGD(p, lr=lr, weight_decay=wd, momentum=momentum)
+
+    base_lr = 0.1
+    scale_factor = batch_size / 128
+
+    warmup_step = 2
+    for i in range(1, int(scale_factor + 1), warmup_step):
+        schedule.add_step(factory=lambda p: new_optim(p, base_lr * i), name=f'Warmup {i / scale_factor:.2f}', duration=1)
+
+    lr = base_lr * scale_factor
+
+    schedule.add_step(factory=lambda p: new_optim(p, lr), name='P1', duration=80)
+    schedule.add_step(factory=lambda p: new_optim(p, lr / 10), name='P2', duration=80)
+    schedule.add_step(factory=lambda p: new_optim(p, lr / 100), name='P3', duration=40)
 
     return schedule
