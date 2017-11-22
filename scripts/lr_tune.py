d80f74630be8c088ee5badc7d08ed04953c99763
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-11-22 09:23:30 -0500

    return lr_tune

--- scripts/lr_tune.py
+++ scripts/lr_tune.py
@@ -8,16 +8,18 @@ from fasttrain.schedules import resnet_paper_schedule
 def run_experiment(batch_size):
     schedule = resnet_paper_schedule(batch_size=batch_size)
 
-    for prob in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-        print(prob)
-        model = ResNetCIFAR(n=20, stochastic_depth={'from': prob, 'to': prob})
-        train_on_cifar(model, schedule, name=f'ResNet(sd-p={prob})', batch_size=batch_size)
+    for lr in [0.5, 0.1, 0.05, 0.01]:
+        for sd in [0.2, 0.4, 0.6, 0.8, 1.0]:
+            model = ResNetCIFAR(n=20, stochastic_depth={'from': sd, 'to': sd})
+            train_on_cifar(model, schedule, name=f'ResNet(lr={lr:.3f}, sd={sd})', batch_size=batch_size)
 
 
 run_experiment(3072)
 run_experiment(2048)
 run_experiment(1024)
-# run_experiment(512)
+# run_experiment(512, multi_gpu=True)
 # run_experiment(256)
 # run_experiment(128)
+
+
 
