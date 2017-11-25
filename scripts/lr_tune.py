04441055a8370d85400af9a20e5c14d8ab50346a
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2017-11-25 14:46:48 -0500

    fix incorrect tuning

--- scripts/lr_tune.py
+++ scripts/lr_tune.py
@@ -6,8 +6,8 @@ from fasttrain.schedules import resnet_paper_schedule
 
 
 def run_experiment(batch_size):
-    schedule = resnet_paper_schedule(batch_size=batch_size)
-    for lr in np.logspace(2, -5, 50):
+    for lr in np.logspace(1, -6, 8):
+        schedule = resnet_paper_schedule(batch_size=batch_size, base_lr=lr)
         sd = 0.5
         model = ResNetCIFAR(n=20, stochastic_depth={'from': sd, 'to': sd})
         train_on_cifar(model, schedule, name=f'ResNet(lr={lr:.2e}, sd={sd})', batch_size=batch_size)
@@ -19,6 +19,3 @@ run_experiment(1024)
 # run_experiment(512, multi_gpu=True)
 # run_experiment(256)
 # run_experiment(128)
-
-
-
