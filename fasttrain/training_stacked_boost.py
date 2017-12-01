dfd66e04dac5c8adcf28f4fcc89d753fe82ea495
Denis Stepanov <denis_v_stepanov@hotmail.com>
2017-12-01 22:07:18 +0300

    print gammas and alphas, remove lr scalling, decrease weight decay

--- fasttrain/training_stacked_boost.py
+++ fasttrain/training_stacked_boost.py
@@ -8,7 +8,7 @@ from fasttrain.data import load_cifar10_boost, SublistDataset
 from fasttrain.model.boostresnet import BoostResNetCIFAR
 
 
-def train_stacked_boost(n, batch_size=128, base_epoch=40, base_lr=0.1, stochastic_depth=None, pre_activated=True, show_test=False):
+def train_stacked_boost(n, batch_size=128, base_epoch=1, base_lr=0.1, stochastic_depth=None, pre_activated=True, show_test=False):
     net = BoostResNetCIFAR(n, pre_activated=pre_activated, stochastic_depth=stochastic_depth)
     print('N = {}'.format(n))
     print('Batch size = {}'.format(batch_size))
@@ -26,25 +26,39 @@ def train_stacked_boost(n, batch_size=128, base_epoch=40, base_lr=0.1, stochasti
     runner = BoostRunner(net, train, dev, batch_size=batch_size, num_classes=10)
     runner.on_epoch(VisdomReporter('{} n={} batch={} pre_activated={} sd={}'.format('ResNet', n, batch_size, pre_activated, stochastic_depth)))
 
-    wd = 0.0001
+    wd = 0.000001
     momentum = 0.9
 
     def optim_factory(lr):
         return lambda p: optim.SGD(p, lr, weight_decay=wd, momentum=momentum)
 
-    lr_scaling = batch_size / 128
+    lr_scaling = 1 #batch_size / 128
     warmup_step = 2
 
+    gammas = []
+    alphas = []
     for t in range(net.layers):
-        print('layer: {}'.format(t))
+        print('Layer: {}'.format(t))
         net.set_layer(t)
 
         for i in range(1, int(lr_scaling + 1), warmup_step):
-            runner.run(optim_factory(i * 0.0001), epochs=10)
+            runner.run(optim_factory(i * 0.1), epochs=10)
 
-        runner.run(optim_factory(base_lr * lr_scaling), epochs=base_epoch*2)
-        runner.run(optim_factory(base_lr / 10 * lr_scaling), epochs=base_epoch*2)
+        runner.run(optim_factory(base_lr * lr_scaling), epochs=base_epoch * 2)
+        runner.run(optim_factory(base_lr / 10 * lr_scaling), epochs=base_epoch)
         runner.run(optim_factory(base_lr / 100 * lr_scaling), epochs=base_epoch)
+        runner.run(optim_factory(base_lr / 1000 * lr_scaling), epochs=base_epoch)
+
+        gamma, alpha = runner.update_alpha()
+        gammas.append(gamma)
+        alphas.append(alpha)
+        print('gamma: {}, alpha: {}'.format(gamma, alpha))
+        print(gammas)
+        print(alphas)
+        eps = 1e-5
+        if t > 0:
+            weak_learning_cond = (gammas[-1]**2-gammas[-2]**2)/(1-gammas[-1]**2 + eps)
+            print('weak learning condition: {}'.format(weak_learning_cond))
 
         dev_acc = runner.evaluate(dev)
         print('Dev accuracy: {}'.format(dev_acc))
